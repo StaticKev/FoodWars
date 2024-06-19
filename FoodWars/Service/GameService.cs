fb231136245c0a2e2d9dadc53000b8a9d98acb88
StaticKev <statickev321@gmail.com>
2024-06-19 15:09:06 +0700

    DUMMY 2

--- FoodWars/Service/GameService.cs
+++ FoodWars/Service/GameService.cs
@@ -15,35 +15,6 @@ namespace FoodWars.Service
 
         private PlayerRepo repo;
 
-        List<Ingredients> riceIngredients = new List<Ingredients>
-        {
-            new Ingredients("Regular Rice", 5_000, IngredientCategory.RICE),
-            new Ingredients("Brown Rice", 10_000, IngredientCategory.RICE),
-        };
-        List<Ingredients> proteinIngredients = new List<Ingredients>
-        {
-            new Ingredients("Tonkatsu", 30_000, IngredientCategory.PROTEIN),
-            new Ingredients("Chicken Katsu", 15_000, IngredientCategory.PROTEIN),
-            new Ingredients("Ebi Furai", 20_000, IngredientCategory.PROTEIN)
-        };
-        List<Ingredients> vegetableIngredients = new List<Ingredients>
-        {
-            new Ingredients("Pickled Radish", 5_000, IngredientCategory.VEGETABLES),
-            new Ingredients("Pickled Cucumber", 5_000, IngredientCategory.VEGETABLES)
-        };
-        List<Ingredients> sideDishesIngredients = new List<Ingredients>
-        {
-            new Ingredients("Sunomono", 7_000, IngredientCategory.SIDE_DISHES),
-            new Ingredients("Nimono", 8_000, IngredientCategory.SIDE_DISHES),
-            new Ingredients("Korokke", 10_000, IngredientCategory.SIDE_DISHES)
-        };
-
-        IngredientsMap availableIngredients = new IngredientsMap();
-        availableIngredients.Add(IngredientCategory.RICE, riceIngredients, riceIngredients.Count);
-        availableIngredients.Add(IngredientCategory.PROTEIN, proteinIngredients, proteinIngredients.Count);
-        availableIngredients.Add(IngredientCategory.VEGETABLES, vegetableIngredients, vegetableIngredients.Count);
-        availableIngredients.Add(IngredientCategory.SIDE_DISHES, sideDishesIngredients, sideDishesIngredients.Count);
-
         public GameService(PlayerRepo repo)
         {
             this.repo = repo;
